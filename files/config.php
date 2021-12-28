e9dd1a4be2b57f8f46cfce8cb86f1b59941215c2
Nicolas Martignoni <nicolas@martignoni.net>
2021-12-28 17:29:21 +0100

    Update and translate doc into English
    - 7th batch of editing; more to come.
    - Moodle configuration.
    - Some formatting fixes.

--- files/config.php
+++ files/config.php
@@ -8,27 +8,37 @@ $CFG->dbtype    = 'mariadb';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'localhost';
 $CFG->dbname    = 'moodle';
-$CFG->dbuser    = 'root';
+$CFG->dbuser    = 'moodlebox';
 $CFG->dbpass    = 'Moodlebox4$';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
   'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
 );
 
 $CFG->wwwroot   = 'http://moodlebox.home';
 $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 
+$CFG->backuptempdir = '/var/www/moodledata/backup';
 $CFG->xsendfile = 'X-Accel-Redirect';
-$CFG->xsendfilealiases = array (
-  '/dataroot/' => $CFG->dataroot
+$CFG->xsendfilealiases = array('/dataroot/' => $CFG->dataroot);
+$CFG->customfiletypes = array(
+  (object)array(
+    'extension' => 'crt',
+    'icon' => 'sourcecode',
+    'type' => 'application/x-x509-ca-cert',
+    'customdescription' => 'X.509 CA certificate'
+  )
 );
+$CFG->showcampaigncontent = false;
+$CFG->showservicesandsupportcontent = false;
 
 $CFG->directorypermissions = 02777;
 
-require_once(dirname(__FILE__) . '/lib/setup.php');
+require_once(__DIR__ . '/lib/setup.php');
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
